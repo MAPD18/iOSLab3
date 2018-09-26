61e663861074db9d2c1d9fe28f3b348705fdb690
Rodrigo Silva <digovs@gmail.com>
2018-09-26 01:17:09 -0400

    Adding Constant and Unary operations

--- Calculator/Calculator/Engine.swift
+++ Calculator/Calculator/Engine.swift
@@ -8,6 +8,10 @@
 
 import Foundation
 
+func multiply(first: Double, second: Double) -> Double {
+        return first * second
+}
+
 class Engine {
     
     private var calculus = 0.0
@@ -16,13 +20,34 @@ class Engine {
         calculus = first
     }
     
+    private var operations: Dictionary = [
+        "∏" : Operation.Constant(.pi),
+        "√" : Operation.Unary(sqrt),
+        "cos" : Operation.Unary(cos),
+        "×" : Operation.Binary(multiply)
+    ]
+    
     func doOperation(mathSymbol: String) {
-        switch mathSymbol {
-        case "√": calculus = sqrt(calculus)
-        case "∏": calculus = .pi
-        default:
-            break;
+        if let operation = operations[mathSymbol] {
+            switch operation {
+            case .Constant(let value) : calculus = value
+            case .Unary(let unaryOperation) : calculus = unaryOperation(calculus)
+            case .Binary(let binaryOperation) : break
+            case .Equals : break
+            }
         }
+    }
+    
+    struct PendingBinaryOperation {
+        var firstNumber : Double
+        var bynaryOperation : (Double, Double) -> Double
+    }
+    
+    enum Operation {
+        case Constant(Double)
+        case Unary((Double) -> Double)
+        case Binary((Double, Double) -> Double)
+        case Equals
     }
     
     var result : Double {
