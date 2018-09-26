d36905a5fe070d9a40f87f09e8443c48aecbf532
Rodrigo Silva <digovs@gmail.com>
2018-09-26 12:25:01 -0400

    Adding binary Operations

--- Calculator/Calculator/Engine.swift
+++ Calculator/Calculator/Engine.swift
@@ -12,9 +12,22 @@ func multiply(first: Double, second: Double) -> Double {
         return first * second
 }
 
+func sum(first: Double, second: Double) -> Double {
+    return first + second
+}
+
+func subtract(first: Double, second: Double) -> Double {
+    return first - second
+}
+
+func divide(first: Double, second: Double) -> Double {
+    return first / second
+}
+
 class Engine {
     
     private var calculus = 0.0
+    private var pending: PendingBinaryOperation?
     
     func setFirstNumber(first: Double) {
         calculus = first
@@ -24,23 +37,39 @@ class Engine {
         "∏" : Operation.Constant(.pi),
         "√" : Operation.Unary(sqrt),
         "cos" : Operation.Unary(cos),
-        "×" : Operation.Binary(multiply)
+        "×" : Operation.Binary(multiply),
+        "÷" : Operation.Binary(divide),
+        "+" : Operation.Binary(sum),
+        "−" : Operation.Binary(subtract),
+        "=" : Operation.Equals
     ]
     
     func doOperation(mathSymbol: String) {
         if let operation = operations[mathSymbol] {
             switch operation {
-            case .Constant(let value) : calculus = value
-            case .Unary(let unaryOperation) : calculus = unaryOperation(calculus)
-            case .Binary(let binaryOperation) : break
-            case .Equals : break
+            case .Constant(let value):
+                calculus = value
+            case .Unary(let unaryOperation):
+                calculus = unaryOperation(calculus)
+            case .Binary(let binaryOperation):
+                executePendingOperation()
+                pending = PendingBinaryOperation(firstNumber: calculus, binaryOperation: binaryOperation)
+            case .Equals:
+                executePendingOperation()
             }
+        }
+    }
+    
+    func executePendingOperation() {
+        if pending != nil {
+            calculus = pending!.binaryOperation(pending!.firstNumber, calculus)
+            pending = nil
         }
     }
     
     struct PendingBinaryOperation {
         var firstNumber : Double
-        var bynaryOperation : (Double, Double) -> Double
+        var binaryOperation : (Double, Double) -> Double
     }
     
     enum Operation {
