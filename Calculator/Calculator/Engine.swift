b67fac41d794dace9ff31be7c1b3bf56dbd2e427
Rodrigo Silva <digovs@gmail.com>
2018-09-26 00:12:52 -0400

    Adding unary operations square roots and pi

---
+++ Calculator/Calculator/Engine.swift
@@ -0,0 +1,22 @@
+//
+//  Engine.swift
+//  Calculator
+//
+//  Created by Rodrigo Silva on 2018-09-25.
+//  Copyright © 2018 Rodrigo Silva. All rights reserved.
+//
+
+import Foundation
+
+class Engine {
+    
+}
+
+enum Operation: Int {
+    case sum = 12
+    case subtract = 13
+    case divide = 14
+    case multiply = 15
+    case equals = 16
+    case decimal = 11
+}
--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var historicLabel: UILabel!
+    
+    var userIsTyping = false
+    var resultValue: Double {
+        get {
+            return Double(resultLabel.text!)!
+        }
+        set {
+            resultLabel.text = String(newValue)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func onDigitPress(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if (userIsTyping) {
+            let currentText = resultLabel.text!
+            resultLabel.text = currentText + digit
+        } else {
+            resultLabel.text = digit
+            userIsTyping = true
+        }
+    }
+    
+    @IBAction func onOperationPress(_ sender: UIButton) {
+        userIsTyping = false
+        let mathSymbol = sender.currentTitle!
+        if mathSymbol == "√" {
+            resultValue = sqrt(resultValue)
+        } else if mathSymbol == "∏" {
+            resultValue = .pi
+        }
+        
+    }
 }
-
