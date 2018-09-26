234bc95f712c7ade0ea131638fe786dc6d2988f5
Rodrigo Silva <digovs@gmail.com>
2018-09-25 21:29:12 -0400

    Setting up XCode Project

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Rodrigo Silva on 2018-09-25.
+//  Copyright © 2018 Rodrigo Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
